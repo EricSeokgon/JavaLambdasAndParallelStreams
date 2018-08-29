a06e28cd58650cdbe28ba3ab77a69634c70d373e
sklee <hadesleeq@gmail.com>
2018-08-29 11:10:04 +0900

    Implementing Class (Without Further Functionality)

--- src/chapter5/InterfaceDemo.java
+++ src/chapter5/InterfaceDemo.java
@@ -1,8 +1,9 @@
 package chapter5;
 
+class InterfaceImplementor implements InterfaceA{
+}
+
 public class InterfaceDemo {
-    private static class InterfaceImplementor implements InterfaceA{
-    }
 
     public static void main(String[] args) {
         new InterfaceImplementor().print();
