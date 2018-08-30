8d1a75707687d78bcf76988d3d92fa85c353d7bd
sklee <hadesleeq@gmail.com>
2018-08-30 11:14:10 +0900

    ArticleInfo

---
+++ src/chapter1/ArticleInfo.java
@@ -0,0 +1,40 @@
+package chapter1;
+
+public class ArticleInfo {
+    private final int _articleNo;
+    private long _quantity;
+    private Money _amount;
+
+    public ArticleInfo(int articleNo) {
+        _articleNo = articleNo;
+        _amount = new Money();
+    }
+
+    public int getArticleNo() {
+        return _articleNo;
+    }
+
+    public long getQuantity() {
+        return _quantity;
+    }
+
+    public void setQuantity(long quantity) {
+        _quantity = quantity;
+    }
+
+    public Money getAmount() {
+        return _amount;
+    }
+
+    public void setAmount(Money amount) {
+        _amount = amountArticleInfo.java;
+    }
+
+    public void addQuantity(long quantity) {
+        _quantity += quantity;
+    }
+
+    public void addPrice(long cents) {
+        _amount.add(cents);
+    }
+}
